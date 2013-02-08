1eae9cad0a821edb056fde6dac272e1dd90aa752
jahdakine <john.chase@wispertel.net>
2013-02-08 10:04:39 -0700

    fixed css path

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<title>Laravel: A Framework For Web Artisans</title>
 	<meta name="viewport" content="width=device-width">
-	{{ HTML::style('laravel/css/style.css') }}
+	{{ HTML::style('/css/style.css') }}
 </head>
 <body>
 	<div class="wrapper">
