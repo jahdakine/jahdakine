a75b069dc1891ac428cf5ef3c3f55167f3a4f07a
jahdakine <john.chase@wispertel.net>
2013-02-08 12:25:39 -0700

    add base method

--- application/controllers/base.php
+++ application/controllers/base.php
@@ -21,5 +21,31 @@ class Base_Controller extends Controller {
 	{
 		return Response::error('404');
 	}
+	/**
+	 * set config/application.php url to the base url of the application. 
+	 * Gets rid of the incorrect guessing of baseurl by the Symfony 
+	 * components
+	 *
+	 * @return base
+	 */	
+	public static function base()
+	{
+		if (isset(static::$base)) return static::$base;
 
+		$base = 'http://localhost';
+
+		// If the application's URL configuration is set, we will just use that
+		// instead of trying to guess the URL from the $_SERVER array's host
+		// and script variables as this is a more reliable method.
+		if (($url = Config::get('application.url')) !== '')
+		{
+			$base = $url;
+		}
+		else
+		{
+			$base = Request::foundation()->getRootUrl();
+		}
+
+		return static::$base = $base;
+	}
 }
