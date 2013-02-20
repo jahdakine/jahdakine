2d04d7b75a520a9e0d0061611ee8153caebe509b
jahdakine <john.chase@wispertel.net>
2013-02-20 08:44:30 -0700

    add other audio formats to 404

--- application/views/error/404.php
+++ application/views/error/404.php
@@ -4,93 +4,91 @@
 	<meta charset="utf-8">
 	<title>Error 404 - Not Found</title>
 	<style type="text/css">
-		@import url(http://fonts.googleapis.com/css?family=Droid+Sans);
-
-		body
-		{
-			font-family:'Droid Sans', sans-serif;
-			font-size:10pt;
-			color:#fff;
-			background-color: #000;
-			background-image:url('/img/404.png');
-			background-repeat:no-repeat;
-			line-height: 25px;
-		}
-
-		.wrapper
-		{
-			width:600px;
-			margin:1em;
-		}
-
-		.main
-		{
-			overflow:hidden;
-		}
-
-		.error-spacer
-		{
-			height:26em;
-		}
-
-		a, a:visited
-		{
-			color:#FF0000;
-		}
-
-		a:hover
-		{
-			color:#00FF00;
-		}
-		img.bg {
-			/* Set rules to fill background */
-			height: 400px;
-			width: 600px;
-			z-index: -1;
-			position: fixed;
-			top: 0;
-			left: 0;
-			border: 0;
-		}
-		#eyes1 {
-			width:75px;
-			height:25px; 			
-			top:322px;
-			left:8px;
-		}
-		#eyes2 {
-			width:17px;
-			height:25px; 			
-			top:244px;
-			left:188px;
-		}
-		#eyes3 {
-			width:50px;
-			height:25px; 			
-			top:215px;
-			left:330px;
-		}				
-		.eyelids {
-			background-color:black;
-			position: absolute;
-			z-index: 5;
-		  -webkit-opacity: 0.25;
-		  -moz-opacity: 0.25;
-		  opacity: 0.25;
-			-webkit-transition: all 1s ease;
-		  -moz-transition: all 1s ease;
-		  -ms-transition: all 1s ease;
-		  -o-transition: all 1s ease;
-		  transition: all 1s ease;
-		} 
-		#eyes1:hover, #eyes2:hover, #eyes3:hover {
-		  -webkit-opacity: 1;
-		  -moz-opacity: 1;
-		  opacity: 1;
-		}
+	@import url(http://fonts.googleapis.com/css?family=Droid+Sans);
+	body{
+		font-family:'Droid Sans', sans-serif;
+		font-size:10pt;
+		color:#fff;
+		background-color: #000;
+		background-image:url('/img/404.png');
+		background-repeat:no-repeat;
+		line-height: 25px;
+	}
+	.wrapper {
+		width:600px;
+		margin:1em;
+	}
+	.main {
+		overflow:hidden;
+	}
+	.error-spacer	{
+		height:26em;
+	}
+	a, a:visited {
+		color:#FF0000;
+	}
+	a:hover {
+		color:#00FF00;
+	}
+	img.bg {
+		/* Set rules to fill background */
+		height: 400px;
+		width: 600px;
+		z-index: -1;
+		position: fixed;
+		top: 0;
+		left: 0;
+		border: 0;
+	}
+	#eyes1 {
+		width:75px;
+		height:25px; 			
+		top:322px;
+		left:8px;
+	}
+	#eyes2 {
+		width:17px;
+		height:25px; 			
+		top:244px;
+		left:188px;
+	}
+	#eyes3 {
+		width:50px;
+		height:25px; 			
+		top:215px;
+		left:330px;
+	}				
+	.eyelids {
+		background-color:black;
+		position: absolute;
+		z-index: 5;
+	  -webkit-opacity: 0.25;
+	  -moz-opacity: 0.25;
+	  opacity: 0.25;
+		-webkit-transition: all 1s ease;
+	  -moz-transition: all 1s ease;
+	  -ms-transition: all 1s ease;
+	  -o-transition: all 1s ease;
+	  transition: all 1s ease;
+	} 
+	#eyes1:hover, #eyes2:hover, #eyes3:hover {
+	  -webkit-opacity: 1;
+	  -moz-opacity: 1;
+	  opacity: 1;
+	}
 	</style>
 </head>
 <body>
+	<audio id="swamp1" preload autoplay>
+		<source src="/audio/swamp1_404.ogg">
+	  <source src="/audio/swamp1_404.wav">
+	  <source src="/audio/swamp1_404.mp3">
+	</audio>
+	<audio id="swamp2" preload autoplay>
+	  <source src="/audio/swamp2_404.ogg">
+	  <source src="/audio/swamp2_404.wav">
+	  <source src="/audio/swamp2_404.mp3">
+	</audio>	
 	<div class="wrapper">
 		<div class="error-spacer"></div>
 		<div role="main" class="main">
@@ -98,7 +96,7 @@
 			<div class="eyelids" id="eyes1"></div>
 			<div class="eyelids" id="eyes2"></div>
 			<div class="eyelids" id="eyes3"></div>
-			<?php $messages = array('Ooh, scary!', 'Umm, I think we\'re lost...', 'Yikes, we took a wrong turn!', 'Looks like we\'re not in Kansas anymore Toto!', 'Help, I\'ve 404\'d and I can\'t get up!'); ?>
+			<?php $messages = array('Ooh, scary!', 'Umm, I think you\'re lost...', 'Yikes, we took a wrong turn!', 'Looks like we\'re not in Kansas anymore Toto!', 'Help, I\'ve 404\'d and I can\'t get up!'); ?>
 
 			<h1><?php echo $messages[mt_rand(0, 4)]; ?></h1>
 
@@ -106,9 +104,23 @@
 
 			<p>
 				We don't know how you got here,<br/>
-				but grab on to this <?php echo HTML::link('/', 'rope'); ?> and we'll pull you back to safety. 
+				but if you grab on to this <?php echo HTML::link('/', 'rope'); ?>, we'll pull you back to safety. 
 			</p>
 		</div>
 	</div>
+	<script src='js/vendor/jquery-1.8.2.min.js'></script>
+	<script>
+	document.getElementById('swamp1').addEventListener('ended', function(){
+		this.currentTime = 0;
+		this.pause();
+		document.getElementById('swamp2').play();
+	}, false);
+ 
+	document.getElementById('swamp2').addEventListener('ended', function(){
+		this.currentTime = 0;
+		this.pause();
+		document.getElementById('swamp1').play();
+	}, false);
+</script>
 </body>
 </html>
