1a5527035abed8eb3a244f4b0d5461130da2b00a
jahdakine <john.chase@wispertel.net>
2013-03-13 11:10:13 -0600

    portfolio noscripts

--- application/views/home/_project.blade.php
+++ application/views/home/_project.blade.php
@@ -1,6 +1,24 @@
-<h3>{{ucfirst($name)}}</h3>
+<?php  
+$newName = $name;
+$words = false;
+if(strpos($name, "_")) {
+	$newName = ucwords(str_replace("_"," ", $name));
+	$words = true;
+}
+if(preg_match('/caps/', $params)) {
+	if($words) {
+ 		$tmp = explode(" ", $newName); $first = array_shift($tmp); $second = join(" ", $tmp); echo "<h3>" .strtoupper($first). " " .$second. "</h3>";
+	} else {
+		strtoupper($newName); echo "<h3>" .strtoupper($newName). "</h3>"; 
+	}
+} else {
+	echo "<h3>" .ucfirst($newName). "</h3>";
+}
+?>
 <div>
-	<img style="max-width:800px;" src="/img/portfolio/{{$name}}.jpg" alt="Screenshot of {{$name}} project" align="right" class="project-img" />
+	@if(!preg_match('/noimg/',$params))
+		<img style="max-width:800px;" src="/img/portfolio/{{$name}}.jpg" alt="Screenshot of {{$name}} project" align="right" class="project-img" />
+	@endif
 </div>
 <div id="project-desc">
 	@include("home.partials.$name")
